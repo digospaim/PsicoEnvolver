86c100a7fecf4fd5604fc2cfc7324eccb75f425c
Rodrigo Paim <rodrigosoarespaim@gmail.com>
2022-09-15 17:53:54 -0300

    Add Backend

---
+++ backend/src/main/java/com/psicoenvolver/crm/PsicoenvolverApplication.java
@@ -0,0 +1,13 @@
+package com.psicoenvolver.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsicoenvolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsicoenvolverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/psicoenvolver/crm/PsicoenvolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psicoenvolver.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsicoenvolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
